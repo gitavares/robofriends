505ad5db036de04893e4d3e29709465f5e01131d
Giselle Tavares <gitavares@hotmail.com>
2019-07-21 19:38:30 -0400

    Updates

---
+++ precache-manifest.97975079734379d7c1e6b75bdc04b902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710a1f2f10314df68393a883fc0609e3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "262a95089cff2f611a64",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e9e9e002254eaa8a2e04",
+    "url": "/robofriends/static/css/main.063c80c7.chunk.css"
+  },
+  {
+    "revision": "262a95089cff2f611a64",
+    "url": "/robofriends/static/js/2.7e9d9553.chunk.js"
+  },
+  {
+    "revision": "e9e9e002254eaa8a2e04",
+    "url": "/robofriends/static/js/main.a2180971.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
